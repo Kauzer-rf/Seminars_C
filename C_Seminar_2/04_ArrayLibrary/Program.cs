f2b6cb436a92f3dc676bc1ffcc51860de797431b
a.fedotov <zacyla@gmail.com>
2022-07-11 18:18:44 +0300

    04 is done

--- C_Seminar_2/04_ArrayLibrary/Program.cs
+++ C_Seminar_2/04_ArrayLibrary/Program.cs
@@ -1,22 +1,28 @@
 ﻿/* Учучшенный синтаксис 03 задачи
 */
 // void - метод который ничего не возвращает
-void FillArray(int[] collection){
-    int lenght = collection.Length;
+void FillArray(int[] col1)
+{
+    int length = col1.Length;
     int index = 0;
-    while (index < length){
-        collection[index] = new Random().Next(1, 10);
+    while (index < length)
+    {
+        col1[index] = new Random().Next(1, 10);
         index++;
     }
 }
-void PrintArray(int[] col){
-    int count = col.Length;
-    int position = 0;
-    while (position < count){
-        Console.WriteLine(col[position]);
-        position++;
+void PrintArray(int[] col2)
+{
+    int lenght2 = col2.Length;
+    int index2 = 0;
+    while (index2 < lenght2)
+    {
+        Console.WriteLine(col2[index2]);
+        index2++;
     }
 }
 
 int[] array = new int[10];
 
+FillArray(array);
+PrintArray(array);
