b8e83fc57dcd28227c446457085491a6b1b8f0bc
a.fedotov <zacyla@gmail.com>
2022-07-12 20:40:40 +0300

    05 zadacha

---
+++ C_Seminar_2/05_Zadacha/Program.cs
@@ -0,0 +1,21 @@
+﻿/**/
+
+int num = new Random().Next(10, 100);
+Console.WriteLine("Random number: " + num ;
+
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+Console.WriteLine("Result " + getMax(firstDigit, secondDigit));
+
+int getMax(int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
