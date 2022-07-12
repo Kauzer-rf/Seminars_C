7644c1619e9668c18aef4763e583d80e065d370c
a.fedotov <zacyla@gmail.com>
2022-07-12 20:55:52 +0300

    5 fixed

--- C_Seminar_2/05_Zadacha/Program.cs
+++ C_Seminar_2/05_Zadacha/Program.cs
@@ -1,7 +1,7 @@
 ﻿/**/
 
 int num = new Random().Next(10, 100);
-Console.WriteLine("Random number: " + num ;
+Console.WriteLine("Random number: " + num);
 
 int firstDigit = num / 10;
 int secondDigit = num % 10;
