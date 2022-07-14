308ddee1dc20f652971b496fc66cfddac8bf1431
a.fedotov <zacyla@gmail.com>
2022-07-14 15:52:05 +0300

    6 done with function

--- C_Seminar_2/06_Zadacha/Program.cs
+++ C_Seminar_2/06_Zadacha/Program.cs
@@ -1,10 +1,16 @@
 ﻿/* Напишите программу, которая выводит 3х значное число и удаляет втроую цифру этого числа.
 */
 
-int a = new Random().Next (100, 1000);
-Console.WriteLine(a);
+int a = new Random().Next(100, 1000);
+Console.WriteLine (a);
 
-int digitone = a / 100;
-int digitThird = a % 10;
-//Console.WriteLine(digitone*10 + digitThird);
-Console.WriteLine(digitone + "" + digitThird);
+int result = delete2num (a);
+Console.WriteLine (result);
+
+int delete2num (int number)
+{
+    int digitOne = number / 100;
+    int digitThird = number % 10;
+    int resultDouble = digitOne * 10 + digitThird;
+    return resultDouble;
+}
