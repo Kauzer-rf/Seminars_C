ed55db7fa22ce894f9a4fddf943cb109b1f8a5b5
a.fedotov <zacyla@gmail.com>
2022-07-14 14:19:50 +0300

    6 done

--- C_Seminar_2/06_Zadacha/Program.cs
+++ C_Seminar_2/06_Zadacha/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая выводит 3х значное число и удаляет втроую цифру этого числа.
+*/
+
+int a = new Random().Next (100, 1000);
+Console.WriteLine(a);
+
+int digitone = a / 100;
+int digitThird = a % 10;
+//Console.WriteLine(digitone*10 + digitThird);
+Console.WriteLine(digitone + "" + digitThird);
