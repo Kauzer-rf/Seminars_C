c625992d21f418f9846171c89ff0b952d7faccda
a.fedotov <zacyla@gmail.com>
2022-07-15 17:04:38 +0300

    8 +method

--- C_Seminar_2/08_Zadacha/Program.cs
+++ C_Seminar_2/08_Zadacha/Program.cs
@@ -4,6 +4,23 @@
 46 -> нет
 161 -> да
 */
+KratnoeChislo(161);
+KratnoeChislo(7);
+KratnoeChislo(1);
+
+void KratnoeChislo(int number)
+{
+    if (number % 7 == 0 && number % 23 == 0)
+    {
+        Console.WriteLine("Число " + number + " кратно и 7, и 23.");
+    }
+    else
+    {
+        Console.WriteLine("Число " + number + " не кратно 7 и 23 одновременно");
+    }
+}
+
+/* Решение без метода
 Console.Write("Enter bumber: ");
 int num = int.Parse(Console.ReadLine());
 
@@ -15,3 +32,4 @@ else
 {
     Console.WriteLine("Число " + num + " не кратно 7 и 23 одновременно");
 }
+*/
