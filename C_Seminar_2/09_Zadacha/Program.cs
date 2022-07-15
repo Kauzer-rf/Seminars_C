3c15ff1df3a6ed1d2fbbfbb1377b148e17a831b3
a.fedotov <zacyla@gmail.com>
2022-07-15 18:24:14 +0300

    9 is done method

--- C_Seminar_2/09_Zadacha/Program.cs
+++ C_Seminar_2/09_Zadacha/Program.cs
@@ -4,16 +4,37 @@
 -4, 16 -> да
 25, 5 -> да
 8,9 -> нет
-*/ 
+*/
 Console.Write("Enter number 1: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Enter number 2: ");
 int num2 = int.Parse(Console.ReadLine());
 
+/* Решение без функции
 if (num1 * num1 == num2 || num2 * num2 == num1)
 {
     Console.WriteLine("Answear: Yes");
 }
 else {
     Console.WriteLine("Answear: No");
+}
+*/
+if (isSqr(num1, num2))
+{
+    Console.WriteLine("Answear: Yes");
+}
+else {
+    Console.WriteLine("Answear: No");
+}
+
+bool isSqr(int num1, int num2)
+{
+    if (num1 * num1 == num2 || num2 * num2 == num1)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
