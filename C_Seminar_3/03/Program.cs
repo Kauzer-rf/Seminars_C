5d383ac461e2d61ccd0bbe972de23609aa94d015
a.fedotov <zacyla@gmail.com>
2022-07-18 16:05:58 +0300

    3 is done

--- C_Seminar_3/03/Program.cs
+++ C_Seminar_3/03/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 18: Напишите программу которая по заданному номеру четверти,
+показывает диапазон возможноых координат точек в этой четверти (х и у).
+*/
+Console.Write("Enter quarter: ");
+int num = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Otvet: " + foundXY(num));
+
+string foundXY(int a)
+{
+    if (a == 1)
+    {
+        return "x > 0 & y > 0";
+    }
+    if (a == 2)
+    {
+        return "x < 0 & y > 0";
+    }
+    if (a == 3)
+    {
+        return "x < 0 & y < 0";
+    }
+    if (a == 4)
+    {
+        return "x > 0 & y < 0";
+    }
+    return "error";
+}
