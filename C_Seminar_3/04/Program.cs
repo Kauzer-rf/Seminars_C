2376c543f3a5f013a98c666fc4c6c7112e2ef0f6
a.fedotov <zacyla@gmail.com>
2022-07-18 16:57:15 +0300

    4 is done

---
+++ C_Seminar_3/04/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09
+A (7,-5); B (1,-1) -> 7,21
+*/
+
+Console.WriteLine("Введите Х1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y1:");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Х2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Y2:");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Otvet: " + getLength(x1,y1,x2,y2));
+
+double getLength(int x1, int y1, int x2, int y2)
+{
+    double Distance = Math.Sqrt(x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+    return Distance;
+}
