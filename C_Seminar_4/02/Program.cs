4aec01ee68e74440f2d37101c02d8c38ab1e1046
a.fedotov <zacyla@gmail.com>
2022-08-04 13:28:35 +0300

    2 way 2 is done

--- C_Seminar_4/02/Program.cs
+++ C_Seminar_4/02/Program.cs
@@ -4,8 +4,19 @@
 456 -> 3
 78 -> 2
 89126 -> 5
-*/ 
+*/
 Console.Write("Введите число: ");
-string num = Console.ReadLine();
+
+/* string num = Console.ReadLine();
 int res = num.Length;
 Console.WriteLine("Количество цифр: " + res);
+*/
+
+int num = int.Parse(Console.ReadLine());
+int i = 0;
+while (num != 0)
+{
+    num = num / 10;
+    i++;
+}
+Console.Write(i);
