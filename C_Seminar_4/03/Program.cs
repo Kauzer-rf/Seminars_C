6a5b248647f3c4391d0cf3be00784ed54a1d2c15
a.fedotov <zacyla@gmail.com>
2022-08-04 13:32:55 +0300

    3 is done

---
+++ C_Seminar_4/03/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Задача 28: Напишите программу, которая
+принимает на вход число N и выдаёт
+произведение чисел от 1 до N.
+4 -> 24
+5 -> 120
+*/
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+int sum = 1;
+
+for (int i = 1; i <= n; i++)
+{
+sum = sum * i;
+}
+Console.WriteLine("Ответ: " + sum);
