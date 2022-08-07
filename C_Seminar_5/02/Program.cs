95f5755e94fbe9e88f86ba2d4c7ca7b47dd3a3fd
a.fedotov <zacyla@gmail.com>
2022-08-07 18:20:45 +0300

    2тв is done

--- C_Seminar_5/02/Program.cs
+++ C_Seminar_5/02/Program.cs
@@ -1,7 +1,28 @@
 ﻿/*
-Задача 30: Напишите программу, которая
-выводит массив из 8 элементов, заполненный
-нулями и единицами в случайном порядке.
-[1,0,1,1,0,1,0,0]
+Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2]
 */
-Console.WriteLine("Hello, World!");
+
+int[] GetArray(int n)
+{
+    int[] arr = new int[n];
+    Random rnd = new Random();
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rnd.Next(-9, 10);
+    }
+    return arr;
+}
+Console.Write("Введите длину массива: ");
+int count = int.Parse(Console.ReadLine());
+int[] arr = GetArray(count);
+Console.WriteLine("Изначальный массив: [{0}", String.Join(", ", arr) + "]");
+
+for (int i = 0; i < arr.Length; i++)
+{
+
+    {
+        arr[i] = arr[i] * (-1);
+    }
+}
+Console.WriteLine("Перевернутый массив: [{0}", String.Join(", ", arr) + "]");
