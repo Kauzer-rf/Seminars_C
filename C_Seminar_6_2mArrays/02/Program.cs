c0ec3b5bb635711c5d5ad6cc00bbd14254254ff7
a.fedotov <zacyla@gmail.com>
2022-08-13 13:23:15 +0300

    2nd is done

--- C_Seminar_6_2mArrays/02/Program.cs
+++ C_Seminar_6_2mArrays/02/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый -
+на последнем и т.д.)
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]
+*/
+int[] array = {1,2,3,4,5,6};
+int tmp = 0;
+for (int i = 0, j = array.Length - 1; i < array.Length /2; i++,j--)
+{
+tmp = array[i];
+array[i] = array[j];
+array[j] = tmp;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write(array[i] + " ");
+}
