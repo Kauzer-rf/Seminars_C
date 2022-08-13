b1a3ee8b23e8beb2f14a3fd33d19439db6871644
a.fedotov <zacyla@gmail.com>
2022-08-13 13:24:37 +0300

    2nd hot fix

--- C_Seminar_6_2mArrays/02/Program.cs
+++ C_Seminar_6_2mArrays/02/Program.cs
@@ -4,13 +4,13 @@
 [1 2 3 4 5] -> [5 4 3 2 1]
 [6 7 3 6] -> [6 3 7 6]
 */
-int[] array = {1,2,3,4,5,6};
+int[] array = { 1, 2, 3, 4, 5, 6 };
 int tmp = 0;
-for (int i = 0, j = array.Length - 1; i < array.Length /2; i++,j--)
+for (int i = 0, j = array.Length - 1; i < array.Length / 2; i++, j--)
 {
-tmp = array[i];
-array[i] = array[j];
-array[j] = tmp;
+    tmp = array[i];
+    array[i] = array[j];
+    array[j] = tmp;
 }
 
 for (int i = 0; i < array.Length; i++)
