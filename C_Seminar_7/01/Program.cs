a3ca046e6bad74068141ce46386f885a89ee3318
a.fedotov <zacyla@gmail.com>
2022-08-15 13:17:00 +0300

    1st is ready

---
+++ C_Seminar_7/01/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+System.Console.Write("Введите количество срок: ");
+int row = int.Parse(Console.ReadLine());
+System.Console.Write("Введите количество столбцов: ");
+int col = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int row, int col)
+{
+    int[,] array = new int[row, col];
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+PrintArray(GetArray(row,col));
