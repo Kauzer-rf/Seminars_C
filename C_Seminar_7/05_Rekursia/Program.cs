b2accb5a81a969980a350440b23da00b23315118
a.fedotov <zacyla@gmail.com>
2022-08-15 18:25:04 +0300

    5 пример рекурсии, нахождение факториала

---
+++ C_Seminar_7/05_Rekursia/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Найти факторичал числа
+*/
+Console.Write("Ведите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Факториал числа " + num + ": " + factorial(num));
+
+int factorial(int number)
+{
+    if (number == 1)
+    {
+        return number;
+    }
+    return number * factorial(number - 1);
+}
