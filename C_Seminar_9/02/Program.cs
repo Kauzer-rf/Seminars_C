d336282d7613900e89608139e0a202c2ad22e452
a.fedotov <zacyla@gmail.com>
2022-08-23 15:51:59 +0300

    2nd is rdy

--- C_Seminar_9/02/Program.cs
+++ C_Seminar_9/02/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 63+: Решить с помощью рекурсии. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 0.
+N = 5 -> "5, 4, 3, 2, 1"
+*/
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine());
+
+void NaturalBack(int start)
+{
+    if (start == -1)
+    {
+        return;
+    }
+    Console.Write(start + ", ");
+    start--;
+    NaturalBack(start);
+}
+NaturalBack(n);
