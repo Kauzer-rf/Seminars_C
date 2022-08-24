d426124c2a97699d4478aaff11c95236661cdd9a
a.fedotov <zacyla@gmail.com>
2022-08-24 13:23:18 +0300

    3rd is rdy

--- C_Seminar_9/03/Program.cs
+++ C_Seminar_9/03/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B
+с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8
+*/
+Console.Write("\nВведите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите степень B: ");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"\nЧисло {A} в степени {B} = {Power(A, B)}");
+
+double Power(int N, int P)
+{
+    if (P == 1)
+    {
+        return N;
+    }
+    if (P == 0)
+    {
+        return 1;
+    }
+    if (P < 0)
+    {
+        return 1 / Power(N, -P);
+    }
+    return N * Power(N, --P);
+}
